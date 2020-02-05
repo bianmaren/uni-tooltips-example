283c200250822a0651238d8c4ef91e24c97988ac
dengwenbing <441889070@qq.com>
2020-02-05 12:23:01 +0800

    demo

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# uni-tooltips-example
-uni-tooltips-example
+# uni-tooltips 工具提示组件DEMO
+兼容微信小程序，h5，uni-app
+
+工具提示组件，为了解决工具提示的问题。
+组件名：uni-tooltips，代码块： uniTooltips。
+
+# 案例
+
+[demo 项目地址：https://github.com/bianmaren/uni-tooltips-example](https://github.com/bianmaren/uni-tooltips-example),
+
+![demo.png](https://file.jxyunge.com/Sw5yEFc3P8mbGBKDBeb7_1580876419165.png)
+
+
+# 使用方式
+在 `script`  中引用组件
+
+```
+import uniTooltips from '@/components/uni-tooltips/uni-tooltips.vue'
+export default {
+    components: {uniTooltips}
+}
+```
+
+在 `template` 中使用组件
+
+```
+<uniTooltips
+    :gravity="gravity"
+    :tooltipShow="tooltipShow"
+    :btns="tooltipBtns"
+    :eleId="eleId"
+    @btnClick="click"></uniTooltips>
+```
+
+# 属性说明
+
+|属性名|类型|默认值|说明|
+|:---|:---:|---:|---:|
+|tooltipShow|Boolean|false|是否显示|
+|textColor|String|#ffffff|字体颜色|
+|backgroundColor|String|#000000|背景颜色|
+|splitColor|String|#ffffff|分隔符颜色|
+|btns|Array|['HelloWorld']|提示里面显示的按钮|
+|eleId|String|空|挂载元素ID|
+|gravity|String|bottom,top,left,right|方向|
+|distance|Number|10|工具提示距离挂载元素距离|
+
+# 事件说明
+
+|事件称名|说明|返回值|
+|:---|:---:|---:|   
+|btnClick|按钮点击|按钮名称|
+
---
+++ pages/index/index.vue
@@ -0,0 +1,83 @@
+<template>
+	<view class="content">
+
+		<view class="demo-wrap">
+			<view id="demo1" class="demo" @click="showTips('demo1','top')">上边提示</view>
+			<view id="demo2" class="demo" @click="showTips('demo2','bottom')">下边提示</view>
+			<view id="demo3" class="demo" @click="showTips('demo3','left')">左边提示</view>
+			<view id="demo4" class="demo" @click="showTips('demo4','right')">右边提示</view>
+		</view>
+
+		<uniTooltips
+			:gravity="gravity"
+			:tooltipShow="tooltipShow"
+			:btns="tooltipBtns"
+			:eleId="eleId"
+			@btnClick="sortTooltipClick"></uniTooltips>
+	</view>
+</template>
+
+<script>
+	
+	//组件倒入
+	import uniTooltips from '@/components/uni-tooltips/uni-tooltips.vue'
+	
+	export default {
+		components: {uniTooltips},
+		data() {
+			return {
+				//方向
+				gravity:'bottom',
+				//是否显示
+				tooltipShow:false,
+				//按钮
+				tooltipBtns:["今日","昨日","近一周","近一月"],
+				//点击元素
+				eleId:""
+			}
+		},
+		onLoad() {
+			
+		},
+		methods: {
+			showTips(id,gravity){
+				this.tooltipShow = true;
+				this.eleId = id;
+				this.gravity = gravity;
+			},
+			sortTooltipClick(btnName){
+				uni.showToast({
+					title:btnName+"点击",
+					icon:'none'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	
+	.demo-wrap{
+		text-align: center;
+		padding-top: 100rpx;
+	}
+	.demo{
+		display: inline-block;
+			border: #000000 solid 1px;
+	}
+	
+	#demo3{
+		position: fixed;
+		right: 0;
+		top: 50%;
+		word-break: break-all;
+		width: 25px;
+	}
+	#demo4{
+		position: fixed;
+		left: 0;
+		top: 50%;
+		word-break: break-all;
+		width: 25px;
+	}
+</style>
